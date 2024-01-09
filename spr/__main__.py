ccd0c90d28ce916fbc64fac395ccd55db07fe389
SOUL X SYSTEM <154720925+mahtoanjali@users.noreply.github.com>
2024-01-09 19:13:53 +0530

    Update __main__.py

--- spr/__main__.py
+++ spr/__main__.py
@@ -50,9 +50,9 @@ async def help_command(_, message: Message):
     kb = ikb(
         {
             "Help": "bot_commands",
-            "Repo": "https://github.com/TheHamkerCat/SpamProtectionRobot",
+            "Repo": "https://github.com/mahtoanjali/AnjaliSpamProtectionRobot",
             "Add Me To Your Group": f"https://t.me/{BOT_USERNAME}?startgroup=new",
-            "Support Chat (for now)": "https://t.me/WBBSupport",
+            "Support Chat (for now)": "https://t.me/MAHTOxOFFICIAL",
         }
     )
     mention = message.from_user.mention
